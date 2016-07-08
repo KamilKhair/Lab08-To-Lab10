b37a1771bb8ca5f50bc79bce7013750aab06f902
KamilKhair <KamilK@code-college.net>
2016-07-08 10:46:37 +0300

    Lab8.2 Publisher - Subscriber Pattern

--- Lab8.2/MailSystem/MailSystem/MailManager.cs
+++ Lab8.2/MailSystem/MailSystem/MailManager.cs
@@ -8,5 +8,33 @@ namespace MailSystem
 {
     public class MailManager
     {
+        public event EventHandler<MailArrivedEventArgs> MailArrived;
+
+        public void SimulateMailArrived()
+        {
+            // render page to printer...
+            OnMailArrived(new MailArrivedEventArgs("title", "body"));
+        }
+        protected virtual void OnMailArrived(MailArrivedEventArgs e)
+        {
+            // C# 6:
+            // MailArrived?.Invoke(this, e);
+
+            if (MailArrived != null)
+            {
+                MailArrived(this, e);
+            }
+        }
+    }
+
+    public class MailArrivedEventArgs : EventArgs
+    {
+        public readonly string Title;
+        public readonly string Body;
+        internal MailArrivedEventArgs(string title, string body)
+        {
+            Title = title;
+            Body = body;
+        }
     }
 }
--- Lab8.2/MailSystem/MailSystem/Program.cs
+++ Lab8.2/MailSystem/MailSystem/Program.cs
@@ -1,15 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
+using System.Threading;
 
 namespace MailSystem
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static MailManager _m;
+        internal static void Main()
         {
+            var mailManager = new MailManager();
+
+            // connecting to the MailArrived event
+            mailManager.MailArrived += Handler;
+            
+            mailManager.SimulateMailArrived();
+
+            // Creating a new System.Threading.TimerCallback delegate object
+            var timerCallback = new TimerCallback(TimerTask);
+
+            // Saving the object globally
+            SetMailManager(mailManager);
+
+            // Creating a new System.Threading.Timer object
+            using (new Timer(timerCallback, null, 1000, 1000))
+            {
+                Thread.Sleep(10000);
+            }
+
+            //var timer = new Timer(o => mailManager.SimulateMailArrived(), null, 1000, 1000);
+        }
+
+        private static void Handler(object sender, MailArrivedEventArgs e)
+        {
+            Console.WriteLine("New Mail has been arrived:");
+            Console.WriteLine($"Title = {e.Title}, Body = {e.Body}");
+            Console.WriteLine();
+        }
+
+        private static void TimerTask(object o)
+        {
+            _m.SimulateMailArrived();
+        }
+
+        private static void SetMailManager(MailManager m2)
+        {
+            _m = m2;
         }
     }
 }
