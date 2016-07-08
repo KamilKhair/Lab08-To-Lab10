1e610d22432312ed6fc63e93c81574f941177940
KamilKhair <KamilK@code-college.net>
2016-07-08 12:44:38 +0300

    Appendix: Operator Overloading

---
+++ Appendix/Rationals/Rationals/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Rationals
+{
+    internal class Program
+    {
+        internal static void Main()
+        {
+            var rationalArr = new[]
+            {
+                new Rational(6, 32),
+                new Rational(2, 12),
+                new Rational(),
+                new Rational(),
+                new Rational(),
+                new Rational()
+            };
+
+            rationalArr[2] = rationalArr[0] + rationalArr[1];
+            rationalArr[3] = rationalArr[0] - rationalArr[1];
+            rationalArr[4] = rationalArr[0] * rationalArr[1];
+            rationalArr[5] = rationalArr[0] / rationalArr[1];
+
+            Console.WriteLine("Before Reduce:");
+            DisplayArr(rationalArr);
+
+            for (var i = 0; i < rationalArr.Length; ++i)
+            {
+                rationalArr[i].Reduce();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("After Reduce:");
+            DisplayArr(rationalArr);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Using the implicit to double casting operator:");
+            var number1 = (double) rationalArr[0];
+            Console.WriteLine($"{rationalArr[0].ToString()} => {number1}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Using the explicit from int casting operator:");
+            var r = (Rational) 8;
+            Console.WriteLine($"8 => {r.ToString()}");
+        }
+
+        private static void DisplayArr(Rational[] rationalArr)
+        {
+            Console.WriteLine(rationalArr[0].ToString() + " + " + rationalArr[1].ToString() + " = " + rationalArr[2].ToString() + " = " + rationalArr[2].Value);
+            Console.WriteLine(rationalArr[0].ToString() + " - " + rationalArr[1].ToString() + " = " + rationalArr[3].ToString() + " = " + rationalArr[3].Value);
+            Console.WriteLine(rationalArr[0].ToString() + " * " + rationalArr[1].ToString() + " = " + rationalArr[4].ToString() + " = " + rationalArr[4].Value);
+            Console.WriteLine(rationalArr[0].ToString() + " / " + rationalArr[1].ToString() + " = " + rationalArr[5].ToString() + " = " + rationalArr[5].Value);
+        }
+    }
+}
---
+++ Appendix/Rationals/Rationals/Rational.cs
@@ -0,0 +1,101 @@
+﻿namespace Rationals
+{
+    internal struct Rational
+    {
+        public Rational(int numerator, int denomirator)
+        {
+            Numerator = numerator;
+            Denomirator = denomirator == 0 ? 1 : denomirator;
+        }
+
+        public Rational(int numerator)
+        {
+            Numerator = numerator;
+            Denomirator = 1;
+        }
+        public int Numerator { get; private set; }
+
+        public int Denomirator { get; private set; }
+
+        public double Value => (double)Numerator / Denomirator;
+
+        public static Rational operator +(Rational r1, Rational r2)
+        {
+            if (r1.Denomirator == r2.Denomirator)
+            {
+                var newRational = new Rational(r1.Numerator + r2.Numerator, r1.Denomirator);
+                return newRational;
+            }
+            else
+            {
+                var denomirator = r1.Denomirator * r2.Denomirator;
+                var newRational = new Rational((denomirator / r2.Denomirator) * r2.Numerator + (denomirator / r1.Denomirator) * r1.Numerator, denomirator);
+                return newRational;
+            }
+        }
+
+        public static Rational operator -(Rational r1, Rational r2)
+        {
+            if (r1.Denomirator == r2.Denomirator)
+            {
+                var newRational = new Rational(r1.Numerator - r2.Numerator, r1.Denomirator);
+                return newRational;
+            }
+            else
+            {
+                var denomirator = r1.Denomirator * r2.Denomirator;
+                var newRational = new Rational((denomirator / r2.Denomirator) * r2.Numerator - (denomirator / r1.Denomirator) * r1.Numerator, denomirator);
+                return newRational;
+            }
+        }
+
+        public static Rational operator *(Rational r1, Rational r2)
+        {
+            var newRational = new Rational(r1.Numerator * r2.Numerator, r1.Denomirator * r2.Denomirator);
+            return newRational;
+        }
+
+        public static Rational operator /(Rational r1, Rational r2)
+        {
+            var newRational = new Rational(r1.Numerator * r2.Denomirator, r1.Denomirator * r2.Numerator);
+            return newRational;
+        }
+
+        public static implicit operator double(Rational r)
+        {
+            return r.Value;
+        }
+
+        public static explicit operator Rational(int num)
+        {
+            return new Rational(num);
+        }
+
+        public override string ToString()
+        {
+            return Numerator + "/" + Denomirator;
+        }
+
+
+        public void Reduce()
+        {
+            var numerator = Numerator;
+            Numerator /= Gcd(Numerator, Denomirator);
+            Denomirator /= Gcd(numerator, Denomirator);
+        }
+
+        private static int Gcd(int a, int b) // Greatest Common Divisor
+        {
+            while (true)
+            {
+                if (b == 0)
+                {
+                    return a;
+                }
+                var r = a % b;
+                a = b;
+                b = r;
+            }
+        }
+    }
+}
