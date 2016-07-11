d2db05f14743e0aa74f1df53f07ed56a0b6f6a68
KamilKhair <KamilK@code-college.net>
2016-07-11 18:34:45 +0300

    Lab10.1 Accessing files

--- Lab10.1/Personnel/Personnel/Program.cs
+++ Lab10.1/Personnel/Personnel/Program.cs
@@ -8,19 +8,35 @@ namespace Personnel
     {
         public static void Main(string[] args)
         {
-            var streamReader = new StreamReader("File.txt");
-            var namesList = ReadAllData(streamReader);
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (var streamReader = new StreamReader("File.txt"))
+                {
+                    var namesList = ReadAllData(streamReader);
 
-            Console.WriteLine("Names Loaded from the file \"File.txt\":");
-            DisplayList(namesList);
+                    Console.WriteLine("Names Loaded from the file \"File.txt\":");
+                    DisplayList(namesList);
 
-            Console.WriteLine("Notice: The file \"File.txt\" is located under the path: ProjectDirectory/bin/Debug");
+                    Console.WriteLine("Notice: The file \"File.txt\" is located under the path: ProjectDirectory/bin/Debug");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public static List<string> ReadAllData(StreamReader streamReader)
         {
             var namesList = new List<string>();
             string line;
+
+            // Read and display lines from the file until the end of 
+            // the file is reached.
             while ((line = streamReader.ReadLine()) != null)
             {
                 namesList.Add(line);
