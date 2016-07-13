216b370a88707efd7f793b357761e947ddf72705
KamilKhair <KamilK@code-college.net>
2016-07-13 23:16:11 +0300

    Advanced-Lab1.3 Create Dynamic Object Provider

---
+++ Advanced-Lab1.3/DynamicXml/DynamicXml/DynamicXElement.cs
@@ -0,0 +1,56 @@
+﻿using System.Dynamic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace DynamicXml
+{
+    public class DynamicXElement : DynamicObject
+    {
+        private DynamicXElement(XElement element)
+        {
+            Element = element;
+        }
+
+        public XElement Element { get; }
+
+        public static dynamic Create(XElement element)
+        {
+            return new DynamicXElement(element);
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            var name = binder.Name;
+
+            var element = Element.Element(name);
+
+            if (element != null && element.IsEmpty)
+            {
+                return base.TryGetMember(binder, out result);
+            }
+            result = new DynamicXElement(element);
+            return true;
+        }
+
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            var name = (string)indexes[0];
+            var index = (int)indexes[1];
+
+            if (name.GetType() != typeof(string) || index.GetType() != typeof(int) || indexes.Length != 2)
+            {
+                result = null;
+                return false;
+            }
+
+            var elements = Element.Elements(name).ToList();
+            result = new DynamicXElement(elements.ElementAt(index));
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return Element.Value;
+        }
+    }
+}
---
+++ Advanced-Lab1.3/DynamicXml/DynamicXml/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace DynamicXml
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                dynamic planets = DynamicXElement.Create(XElement.Load("planets.xml"));
+                var mercury = planets.Planet;       // first planet
+                Console.WriteLine(mercury);
+                var venus = planets["Planet", 1];   // second planet 
+                Console.WriteLine(venus);
+                var ourMoon = planets["Planet", 2].Moons.Moon;
+                Console.WriteLine(ourMoon);
+                var marsMoon = planets["Planet", 3]["Moons", 0].Moon; // mars second moon
+                Console.WriteLine(marsMoon);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
